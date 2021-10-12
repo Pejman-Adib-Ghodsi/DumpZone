424f1385ca7395088492525f8e9905e3ff0ef6c3
Pejman Adib Ghodsi <63265453+Pejman-Adib-Ghodsi@users.noreply.github.com>
2021-10-13 01:43:08 +0200

    Add Category.java from Domain folder

---
+++ Category.java
@@ -0,0 +1,36 @@
+package be.computershop.computershopapi.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Exclude
+    private Long id;
+
+    @Column(unique = true)
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @JsonIgnore
+    private Set<Product> products;
+
+    public Category(String name, Set<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
+}
