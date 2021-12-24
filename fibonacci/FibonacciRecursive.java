e50f079e2cfa5f22be64fbb20da13cc9abc1e070
Pejman Adib Ghodsi <63265453+Pejman-Adib-Ghodsi@users.noreply.github.com>
2021-12-24 19:17:14 +0100

    Adding Fibonacci

---
+++ fibonacci/FibonacciRecursive.java
@@ -0,0 +1,25 @@
+package com.example.fibonacci;
+
+public class FibonacciRecursive {
+
+    static int n1 = 0, n2 = 1, n3;
+
+    public static void calculateFibonacci(int number) {
+        if(number > 0) {
+            n3 = n1+n2;
+            n1= n2;
+            n2= n3;
+            System.out.print(", " + n3);
+            calculateFibonacci(number-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 10;
+
+        System.out.print(n1 + ", " + n2);
+
+        calculateFibonacci(number-2);
+    }
+
+}
