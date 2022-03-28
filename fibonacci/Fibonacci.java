55f449544de3312005e0f725b91d656a0fe64209
Pejman Adib Ghodsi <63265453+Pejman-Adib-Ghodsi@users.noreply.github.com>
2022-03-29 00:55:33 +0200

    Update Fibonacci.java

--- fibonacci/Fibonacci.java
+++ fibonacci/Fibonacci.java
@@ -3,13 +3,13 @@ package com.example.fibonacci;
 public class Fibonacci {
 
     public static void calculateFibonacciSeriesOf(int number){
-        int n1=0, n2=1, n3;
+        int n1 = 0, n2 = 1, n3;
 
         System.out.print(n1 + ", " + n2);
         for(int i=2; i<number; i++) {
             n3 = n1 + n2;
-            n1=n2;
-            n2=n3;
+            n1 = n2;
+            n2 = n3;
             System.out.print(", " + n3);
         }
     }
